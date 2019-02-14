edcc9715c1672aff87e5b105bce58007a0ecd75c
ivan4000 <sxln1@mail.ru>
2019-02-14 21:45:08 +0300

    test commit

--- src/main/java/com/task/taskservice/NotAGUIDException.java
+++
@@ -1,9 +0,0 @@
-package com.task.taskservice;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "This is not a GUID")
-public class NotAGUIDException extends RuntimeException{
-
-
-}
